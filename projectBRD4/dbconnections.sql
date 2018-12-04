21c33a6e75d9225536996177404ffdbd8bafd431
Ganesharun96 <44750297+Ganesharun96@users.noreply.github.com>
2018-12-04 10:35:03 +0530

    Add files via upload

---
+++ projectBRD4/dbconnections.sql
@@ -0,0 +1,16 @@
+--------------------------------------------------------
+--  File created - Wednesday-December-05-2018   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table DBCONNECTIONS
+--------------------------------------------------------
+
+  CREATE TABLE "HR"."DBCONNECTIONS" 
+   (	"NAME" VARCHAR2(20 BYTE), 
+	"PASSWORD" VARCHAR2(20 BYTE), 
+	"MAKERORCHECKER" VARCHAR2(20 BYTE)
+   ) SEGMENT CREATION IMMEDIATE 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS" ;
